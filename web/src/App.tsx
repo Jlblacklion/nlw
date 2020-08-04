80bff0a95b9799e8e48d8762ffb0f0ff380d3997
joel <joel.ad.manuel@gmail.com>
2020-08-04 02:08:39 -0300

    inicializando o projeto com React

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './assets/styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world! </h1>
+
+      <button className="btn btn-">
+        Vamos Cadastrar
+      </button>
+    </div>
+  );
+}
+
+export default App;
