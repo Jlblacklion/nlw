c3ab7a5388376930bf7e4fa3ac7620784f4cd998
joel <joel.ad.manuel@gmail.com>
2020-08-06 16:15:24 -0300

    criando os componetes de teacher itens

--- web/src/components/teacherItens/index.tsx
+++ web/src/components/teacherItens/index.tsx
@@ -2,37 +2,39 @@ import React from 'react'
 
 import WhatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import './style.css'
+
 
 function TeacherItem() {
     return (
         <article className="teacher-item">
-                    <header>
-                        <img src=" https://avatars0.githubusercontent.com/u/41456182?s=460&u=c1937c37c93a89fecf5f0081cfe4d81fc931effa&v=4" alt="Brasta"/>
-                        <div>
-                            <strong> Brasta Jah </strong>
-                            <span> História </span>
-                        </div>
+            <header>
+                <img src=" https://avatars0.githubusercontent.com/u/41456182?s=460&u=c1937c37c93a89fecf5f0081cfe4d81fc931effa&v=4" alt="Brasta"/>
+                <div>
+                    <strong> Brasta Jah </strong>
+                    <span> História </span>
+                </div>
 
-                        <p>
-                            Conheceu a história do mundo aos 15 anos
-                            <br /> <br/>
-                            Antes mesmo de ele saber até onde ele iria e como conhecerias 
-                            as coisas ele já sabia muitas verdades que hoje nem mesmo os estudiosos sabem
-                        </p>
+                <p>
+                    Conheceu a história do mundo aos 15 anos
+                    <br /> <br/>
+                    Antes mesmo de ele saber até onde ele iria e como conhecerias 
+                    as coisas ele já sabia muitas verdades que hoje nem mesmo os estudiosos sabem
+                </p>
 
-                        <footer>
-                            <p>
-                                Preço/hora 
-                                <strong> R$ 80,00 </strong>
-                            </p>
+                <footer>
+                    <p>
+                        Preço/hora 
+                        <strong> R$ 80,00 </strong>
+                    </p>
 
-                            <button type="button">
-                                <img src={WhatsappIcon} alt="whatsapp"/>
-                                Entrar em Contato
-                            </button>
-                        </footer>
-                    </header>
-                </article>
+                    <button type="button">
+                        <img src={WhatsappIcon} alt="whatsapp"/>
+                        Entrar em Contato
+                    </button>
+                </footer>
+            </header>
+        </article>
     )
 }
 
